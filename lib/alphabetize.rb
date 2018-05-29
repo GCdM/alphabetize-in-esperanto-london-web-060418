449ae8819ac968ebbe70d6a55b6fdadbfb837624
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-29 16:46:13 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   range = 4..0
-  (range.first).downto(range.last)
+  (range.first).downto(range.last).each do |i|
     arr.sort_by {
       |string| esperanto_alphabet_order(string[i])
     }

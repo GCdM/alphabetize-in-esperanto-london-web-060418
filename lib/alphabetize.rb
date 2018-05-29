f8169ef44de220d89d1555cb71a270620a9a5b46
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-29 17:22:28 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   sorted_arr = []
-  arr.each_with_index do |string, i|
+  arr.each do |string|
     sorted_arr.each_with_index do |sorted, index|
       test_word = string.to_a[0]
       test_sort = sorted.to_a[0]
@@ -8,9 +8,8 @@ def alphabetize(arr)
       case compare_chars(test_word, test_sort)
         when -1
           sorted_arr.insert(index, string)
-        when 0
           
-        when 1
+        when 0
           
       end
     end
@@ -26,10 +25,6 @@ def compare_chars(a, b)
   esperanto_alphabet_order(a) <=> esperanto_alphabet_order(b)
 end
 
-  test_array = arr.collect {|str| join_string(str)}
-  range = 2..0
-  (range.first).downto(range.last).each do |i|
-    arr.sort_by! do |string|
-      esperanto_alphabet_order(string[i])
-    end
-  end
+def compare_words(word1, word2)
+  
+end

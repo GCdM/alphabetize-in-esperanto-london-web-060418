83fcd42c9a7a86dedd65e8fbf8f5e34c3fa0be6f
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-29 15:35:12 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,7 @@
 def alphabetize(arr)
-  # code here
+  arr.sort_by! {|string| string[0] <=> esperanto_alphabet}
+end
+
+def esperanto_alphabet
+  esperanto_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
 end

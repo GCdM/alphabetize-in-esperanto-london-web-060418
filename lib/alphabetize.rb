238e94903e27cf3a5bbba45b7a8e1a21720ef60d
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-29 17:10:19 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,9 @@
 def alphabetize(arr)
-  range = 2..0
-  (range.first).downto(range.last).each do |i|
-    arr.sort_by! do |string|
-      esperanto_alphabet_order(string[i])
+  sorted_arr = []
+  arr.each_with_index do |string, index|
+    test_str = string.to_a
+    sorted_arr.each_with_index do |sorted, index|
+      
     end
   end
 end
@@ -12,6 +13,14 @@ def esperanto_alphabet_order(char)
   return esperanto_array.index(char)
 end
 
-def join_string(string)
-  string = string.join(" ")
+def compare_chars(a, b)
+  esperanto_alphabet_order(a) <=> esperanto_alphabet_order(b)
 end
+
+  test_array = arr.collect {|str| join_string(str)}
+  range = 2..0
+  (range.first).downto(range.last).each do |i|
+    arr.sort_by! do |string|
+      esperanto_alphabet_order(string[i])
+    end
+  end
